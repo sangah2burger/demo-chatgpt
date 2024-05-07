38ef537eff1318107a17d6b4ba442e807d3b3cc5
sangah2burger <166347765+sangah2burger@users.noreply.github.com>
2024-05-07 08:46:31 +0000

    update add and delete function

--- todo/myapp/routes/index.js
+++ todo/myapp/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
+// Add this line to use express.urlencoded middleware
+router.use(express.urlencoded({ extended: true }));
+
 router.get('/', (req, res) => {
     res.render('home');
 });
@@ -13,6 +16,22 @@ router.get('/about', (req, res) => {
 router.get('/todo', (req, res) => {
     const todos = JSON.parse(fs.readFileSync('./todos.json', 'utf8'));
     res.render('todo', { todos });
+});
+
+// Add a new todo
+router.post('/todo/add', (req, res) => {
+    const todos = JSON.parse(fs.readFileSync('./todos.json', 'utf8'));
+    todos.push(req.body.newTodo);
+    fs.writeFileSync('./todos.json', JSON.stringify(todos));
+    res.redirect('/todo');
+});
+
+// Delete a todo
+router.post('/todo/delete', (req, res) => {
+    const todos = JSON.parse(fs.readFileSync('./todos.json', 'utf8'));
+    const updatedTodos = todos.filter(todo => todo !== req.body.todoToDelete);
+    fs.writeFileSync('./todos.json', JSON.stringify(updatedTodos));
+    res.redirect('/todo');
 });
 
 module.exports = router;
